5c956e4b6bf3f16b73f665884e22b94b2f3e80c6
RYUICHIKITAYA\kitay <ryukita0606@gmail.com>
2024-01-31 15:49:21 +0900

    カテゴリー検索機能最新版

--- app/Http/Controllers/Authenticated/BulletinBoard/PostsController.php
+++ app/Http/Controllers/Authenticated/BulletinBoard/PostsController.php
@@ -29,8 +29,9 @@ class PostsController extends Controller
             ->orWhere('post', 'like', '%'.$request->keyword.'%')->get();
         }else if($request->category_word){
             $sub_category = $request->category_word;
-            $posts = Post::with('user', 'postComments')
-            ->where('sub_category', 'like', '%'.$request->category_word.'%')->get();
+            $posts = Post::with(['sub_category' => function ($query){
+                $query->where('sub_category', 'like', '%'.$request->category_word.'%');
+            }])->get();
         }else if($request->like_posts){
             $likes = Auth::user()->likePostId()->get('like_post_id');
             $posts = Post::with('user', 'postComments')
