cc667e5e994f08e961c8f6ef304794297a310601
RYUICHIKITAYA\kitay <ryukita0606@gmail.com>
2024-02-16 15:58:54 +0900

    カレンダーの表示形式変更

--- app/Calendars/General/CalendarView.php
+++ app/Calendars/General/CalendarView.php
@@ -12,11 +12,11 @@ class CalendarView{
   }
 
   public function getTitle(){
-    return $this->carbon->format('Y年n月');
+    return $this->carbon->format('Y年n月');//予約確認画面で表示されている年月
   }
 
   function render(){
-    $html = [];
+    $html = [];//予約画面上部で表示されている1週間
     $html[] = '<div class="calendar text-center">';
     $html[] = '<table class="table">';
     $html[] = '<thead>';
@@ -31,24 +31,25 @@ class CalendarView{
     $html[] = '</tr>';
     $html[] = '</thead>';
     $html[] = '<tbody>';
-    $weeks = $this->getWeeks();
-    foreach($weeks as $week){
-      $html[] = '<tr class="'.$week->getClassName().'">';
+    $weeks = $this->getWeeks();//getWeeks()メソッドを取得
+    foreach($weeks as $week){//対象月が何週間あるか
+      $html[] = '<tr class="'.$week->getClassName().'">';//繰り返し処理されている1枠分のクラス名？
 
-      $days = $week->getDays();
+      $days = $week->getDays();//1週間から日付を取得
       foreach($days as $day){
-        $startDay = $this->carbon->copy()->format("Y-m-01");
-        $toDay = $this->carbon->copy()->format("Y-m-d");
+        $startDay = $this->carbon->copy()->format("Y-m-01");//月初の初め日付を定義
+        $toDay = $this->carbon->copy()->format("Y-m-d");//今日の日付を定義
 
-        if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){
-          $html[] = '<td class="calendar-td">';
+        if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){//今日より過去か未来か？、、
+          $html[] = '<td class="calendar-td" style="background:#D3D3D3">';//過去スタイルを変更し、薄い灰色に。
         }else{
-          $html[] = '<td class="calendar-td '.$day->getClassName().'">';
+          $html[] = '<td class="calendar-td '.$day->getClassName().'">';//未来
         }
-        $html[] = $day->render();
+        $html[] = $day->render();//繰り返されたひとつひとつをレンダーで表示している。
+        //ここまででカレンダーの記述は完了。
 
-        if(in_array($day->everyDay(), $day->authReserveDay())){
-          $reservePart = $day->authReserveDate($day->everyDay())->first()->setting_part;
+        if(in_array($day->everyDay(), $day->authReserveDay())){//毎日かつ自分が予約している日
+          $reservePart = $day->authReserveDate($day->everyDay())->first()->setting_part;//自分がその日何部で予約しているのか
           if($reservePart == 1){
             $reservePart = "リモ1部";
           }else if($reservePart == 2){
@@ -56,14 +57,16 @@ class CalendarView{
           }else if($reservePart == 3){
             $reservePart = "リモ3部";
           }
-          if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){
-            $html[] = '<p class="m-auto p-0 w-75" style="font-size:12px"></p>';
+          if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){//予約している過去日薄く予約してたやつを出す。
+            $html[] = '<p class="m-auto p-0 w-75" style="font-size:12px" value="'. $day->authReserveDate($day->everyDay())->first()->setting_reserve .'">'. $reservePart .'</p>';
             $html[] = '<input type="hidden" name="getPart[]" value="" form="reserveParts">';
-          }else{
+          }else{//予約している未来日
             $html[] = '<button type="submit" class="btn btn-danger p-0 w-75" name="delete_date" style="font-size:12px" value="'. $day->authReserveDate($day->everyDay())->first()->setting_reserve .'">'. $reservePart .'</button>';
             $html[] = '<input type="hidden" name="getPart[]" value="" form="reserveParts">';
           }
-        }else{
+        }else if($startDay <= $day->everyDay() && $toDay >= $day->everyDay()){
+          $html[] = '<p class="m-auto p-0 w-75" style="font-size:12px" value="">受付終了</p>';
+        }else{//予約していない未来日。
           $html[] = $day->selectPart($day->everyDay());
         }
         $html[] = $day->getDate();
@@ -95,3 +98,8 @@ class CalendarView{
     return $weeks;
   }
 }
+
+//2月は何週間あるの？→5週間で巣となった場合、$weeksは5回繰り返しされる。
+//$weeksを繰り返ししているので、$weeks as $weekで繰り返す。
+//1週間は7日なので、繰り返している週の中に7日で入ってくる。
+//
