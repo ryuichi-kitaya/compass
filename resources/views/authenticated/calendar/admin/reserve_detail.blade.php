e37c2858445627bda53abaf7abc59440c6999512
RYUICHIKITAYA\kitay <ryukita0606@gmail.com>
2024-03-05 15:55:43 +0900

    最新

--- resources/views/authenticated/calendar/admin/reserve_detail.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_detail.blade.php
@@ -3,12 +3,13 @@
 @section('content')
 <div class="vh-100 d-flex" style="align-items:center; justify-content:center;">
   <div class="w-50 m-auto h-75">
-    <p><span>日</span><span class="ml-3">部</span></p>
+    <p><span>{{ $reservePersons->setting_reserve}}</span><span class="ml-3">{{ $reservePersons->setting_part}}部</span></p>
     <div class="h-75 border">
       <table class="">
         <tr class="text-center">
           <th class="w-25">ID</th>
           <th class="w-25">名前</th>
+          <th class="w-25">場所</th>
         </tr>
         <tr class="text-center">
           <td class="w-25"></td>
